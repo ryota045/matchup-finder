4a17fba9a7158aba246f960ea918820ffdd0b56e
ryota <ryota.t0459@gmail.com>
2025-03-01 21:27:13 +0900

    fix: YouTubePlayerコンポーネントのレスポンシブ対応と表示制御を改善

--- frontend/src/components/player/YouTubePlayer.tsx
+++ frontend/src/components/player/YouTubePlayer.tsx
@@ -5,14 +5,10 @@ import { createEmbedUrl } from '../utils/YouTubeUtils';
  * YouTubeプレーヤーコンポーネントのプロパティ
  * @interface YouTubePlayerProps
  * @property {string} url - YouTube動画のURL
- * @property {number|string} [width='100%'] - プレーヤーの幅
- * @property {number|string} [height='100%'] - プレーヤーの高さ
  * @property {boolean} [autoplay=false] - 自動再生するかどうか
  */
 interface YouTubePlayerProps {
   url: string;
-  width?: number | string;
-  height?: number | string;
   autoplay?: boolean;
 }
 
@@ -32,8 +28,6 @@ interface YouTubePlayerProps {
  */
 const YouTubePlayer: React.FC<YouTubePlayerProps> = ({
   url,
-  width = '100%',
-  height = '100%',
   autoplay = false,
 }) => {
   const [embedUrl, setEmbedUrl] = useState<string>('');
